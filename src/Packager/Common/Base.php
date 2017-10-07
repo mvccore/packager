ea0239b74808dd40c3ca5c5364870e3be09ae9fb
Tom Flídr <tomflidr@gmail.com>
2017-10-07 21:50:54 +0200

    bugfix - cli apps compilation

--- src/Packager/Common/Base.php
+++ src/Packager/Common/Base.php
@@ -296,36 +296,49 @@ class Packager_Common_Base {
 		return $result;
 	}
 	public static function ErrorHandler ($severity = NULL, $message = NULL, $file = NULL, $line = NULL, $context = NULL) {
-
-		$backTrace = debug_backtrace();
-		foreach ($backTrace as & $backTraceItem) {
-			unset($backTraceItem['args'], $backTraceItem['object']);
-		}
-		self::$instance->errorHandlerData = func_get_args();
-		self::$instance->exceptionsTraces = $backTrace;
-
-		if (isset($backTrace[count($backTrace) - 2])) {
-			$semiFinalBacktraceRec = (object) $backTrace[count($backTrace) - 2];
-			if ($semiFinalBacktraceRec->class == 'Packager_Php_Completer' && $semiFinalBacktraceRec->function == 'autoloadJob') {
-				header("HTTP/1.1 200 OK");
-				$response = (object) array(
-					'success'			=> 2,
-					'includedFiles'		=> Packager_Php_Scripts_Dependencies::CompleteIncludedFilesByTargetFile(),
-					'exceptionsMessages'=> self::$instance->exceptionsMessages,
-					'exceptionsTraces'	=> self::$instance->exceptionsTraces,
-					'content'			=> '',
-				);
-				self::$instance->sendJsonResultAndExit($response);
-			}
-		}
+		
 	}
 	public static function ExceptionHandler (/*\Exception */$exception = NULL, $exit = TRUE) {
-		if (!is_null($exception)) var_dump($exception);
+		// if (!is_null($exception)) var_dump($exception);
 	}
 	public static function ShutdownHandler () {
-		$exception = error_get_last();
-		if (!is_null($exception)) var_dump($exception);
 		//var_dump(get_included_files());
+		/** @var $exception Exception */
+		$exception = error_get_last();
+		if (!is_null($exception)) {
+			self::$instance->errorHandlerData = $exception->getMessage();
+			self::$instance->exceptionsTraces = $exception->getTrace();
+			header("HTTP/1.1 200 OK");
+			$response = (object) array(
+				'success'			=> 3,
+				'includedFiles'		=> Packager_Php_Scripts_Dependencies::CompleteIncludedFilesByTargetFile(),
+				'exceptionsMessages'=> self::$instance->exceptionsMessages,
+				'exceptionsTraces'	=> self::$instance->exceptionsTraces,
+				'content'			=> '',
+			);
+			self::$instance->sendJsonResultAndExit($response);
+		} else {
+			$backTrace = debug_backtrace();
+			foreach ($backTrace as & $backTraceItem) {
+				unset($backTraceItem['args'], $backTraceItem['object']);
+			}
+			self::$instance->errorHandlerData = func_get_args();
+			self::$instance->exceptionsTraces = $backTrace;
+			if (isset($backTrace[count($backTrace) - 2])) {
+				$semiFinalBacktraceRec = (object) $backTrace[count($backTrace) - 2];
+				if ($semiFinalBacktraceRec->class == 'Packager_Php_Completer' && $semiFinalBacktraceRec->function == 'autoloadJob') {
+					header("HTTP/1.1 200 OK");
+					$response = (object) array(
+						'success'			=> 2,
+						'includedFiles'		=> Packager_Php_Scripts_Dependencies::CompleteIncludedFilesByTargetFile(),
+						'exceptionsMessages'=> self::$instance->exceptionsMessages,
+						'exceptionsTraces'	=> self::$instance->exceptionsTraces,
+						'content'			=> '',
+					);
+					self::$instance->sendJsonResultAndExit($response);
+				}
+			}
+		}
 	}
 	/************************************* dynamic ************************************/
 	protected function shrinkPhpCode (& $code = '') {
